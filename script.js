2fdb0827947fe14e6e65fc24e2b86b343dcb0f11
jonym7293-alt <jonym7293@gmail.com>
2026-02-23 22:36:24 +0600

    add job count calculation and filter toggle functionality

--- script.js
+++ script.js
@@ -12,3 +12,71 @@ const totalCount = document.getElementById('total');
 const interviewCount = document.getElementById('interviewCount');
 const rejectedCount = document.getElementById('rejectedCount');
 const availableJobsCount = document.getElementById('jobsCount');
+
+function calculateCounts(){
+  // totalCount.innerText = allCardSection.children.length;
+  const total = allCardSection.children.length;
+  const interview = interviewList.length;
+  const rejected = rejectedList.length;
+
+  totalCount.innerText = total;
+  interviewCount.innerText = interview;
+  rejectedCount.innerText = rejected;
+
+  let current = 0;
+
+  if(currentStatus === "all-filter-btn"){
+    current = total;
+  }
+  else if(currentStatus === "interview-filter-btn"){
+    current = interview;
+  }
+  else if(currentStatus === "rejected-filter-btn"){
+    current = rejected;
+  }
+
+  availableJobsCount.innerText = `${current} of ${total}`;
+}
+
+function updateCounts(){
+  totalCount.innerText = allCardSection.children.length;
+  availableJobsCount.innerText =  `${allCardSection.children.length} `;
+}
+updateCounts();
+
+
+//all toggling btn section
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+// toggle btn functionality
+function toggleBtn(id){
+//add
+const buttons = [allFilterBtn, interviewFilterBtn, rejectedFilterBtn];
+    
+   buttons.forEach(btn => {
+        btn.classList.remove('bg-linear-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+        btn.classList.add('bg-white', 'text-black');
+   });
+
+    const selected = document.getElementById(id);
+    selected.classList.add('bg-linear-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+    selected.classList.remove('bg-white', 'text-black');
+
+  if(id == 'interview-filter-btn'){
+    allCardSection.classList.add('hidden');
+    filterSection.classList.remove('hidden');
+    renderInterview();
+  }else if(id == 'all-filter-btn'){
+    allCardSection.classList.remove('hidden');
+    filterSection.classList.add('hidden');
+  }else if(id == 'rejected-filter-btn'){
+    allCardSection.classList.add('hidden');
+    filterSection.classList.remove('hidden');
+     renderRejected();
+  }
+
+  currentStatus = id; 
+  calculateCounts();  
+}
