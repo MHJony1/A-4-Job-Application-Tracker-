3d7264d5425d1f4f4cfbfa8bd162e8b87007649d
jonym7293-alt <jonym7293@gmail.com>
2026-02-23 22:30:45 +0600

    Initialize job tracker state variables and DOM element references

---
+++ script.js
@@ -0,0 +1,14 @@
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = "all";
+
+//all container section
+const allCardSection = document.getElementById('allCard');
+const mainContainer = document.querySelector('main');
+const filterSection = document.getElementById('filter-section');
+
+// count section 
+const totalCount = document.getElementById('total');
+const interviewCount = document.getElementById('interviewCount');
+const rejectedCount = document.getElementById('rejectedCount');
+const availableJobsCount = document.getElementById('jobsCount');
